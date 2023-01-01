7e417ef276012d9b4e37daf02014279f74822e5e
360MEKA <bnymncliskn1903@gmail.com>
2023-01-01 23:01:28 +0300

    ads

---
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Areas/Admin/Controllers/ActionRezervationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SfiziAmerica.BusinessLayer.Repository.Concrete;
+using SfiziAmerica.DataAccessLayer.ModelContext;
+using System.Threading.Tasks;
+
+namespace SfiziAmerica.WebUIandUX.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ActionRezervationController : Controller
+    {
+        private DbContext context = new SfizilDatabaseModelContext();
+        private UnitOfWork unitOfWork;
+        public ActionRezervationController()
+        {
+            unitOfWork = new(context);
+        }
+
+
+        [Route("admin/rezervasyon-listele")]
+        public async Task<IActionResult> Index()
+        {
+            var model = await unitOfWork.rezervationRepository.GetAllAsync();
+            return View(model);
+        }
+    }
+}
