f9a5b3d19f5b64d9100599309cf59bf32410b238
360MEKA <bnymncliskn1903@gmail.com>
2023-01-08 11:36:02 +0300

    asd

---
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Areas/Admin/Controllers/ActionMenuCategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SfiziAmerica.BusinessLayer.Repository.Concrete;
+using SfiziAmerica.DataAccessLayer.ModelContext;
+using System;
+using System.Threading.Tasks;
+
+namespace SfiziAmerica.WebUIandUX.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ActionMenuCategoryController : Controller
+    {
+        private DbContext context = new SfizilDatabaseModelContext();
+        private UnitOfWork unitOfWork;
+        public ActionMenuCategoryController()
+        {
+            unitOfWork = new(context);
+        }
+
+        [Route("admin/menü-kategori-listele")]
+        public async Task<IActionResult> Index()
+        {
+            var model = await unitOfWork.menuCategoryRepository.GetAllAsync(null,x=>x.ParentMenuCategory);
+            return View(model);
+        }
+
+
+        [Route("admin/menü-kategori-sil/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var fle = await unitOfWork.menuCategoryRepository.GetAsync(x => x.ID == id);
+            var model = await unitOfWork.menuCategoryRepository.GetAllAsync(x => x.MenuCategoryID == fle.ID);
+            if (fle == null)
+                return NotFound();
+            if (System.IO.File.Exists("wwwroot/Image/Menu/" + fle.ImageUrl1))
+                System.IO.File.Delete("wwwroot/Image/Menu/" + fle.ImageUrl1);
+            if (System.IO.File.Exists("wwwroot/Image/Menu/" + fle.ImageUrl2))
+                System.IO.File.Delete("wwwroot/Image/Menu/" + fle.ImageUrl2);
+            await unitOfWork.menuCategoryRepository.DeleteAsync(fle);
+            await unitOfWork.SaveAsync();
+            return Ok();
+        }
+    }
+}
