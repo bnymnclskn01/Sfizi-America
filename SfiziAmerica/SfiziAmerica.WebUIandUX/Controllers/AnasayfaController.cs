fc8910191d517054b61752f95753c6e794368d1f
360MEKA <bnymncliskn1903@gmail.com>
2023-01-10 18:28:41 +0300

    Düzenleme Alındı

---
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Controllers/AnasayfaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SfiziAmerica.WebUIandUX.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        [Route("")]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
