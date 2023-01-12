df1488e8554e3fbf7d12e28c7186c4906c2033a7
360MEKA <bnymncliskn1903@gmail.com>
2023-01-12 13:05:00 +0300

    Etkinlik Finished

--- SfiziAmerica/SfiziAmerica.WebUIandUX/Controllers/EtkinlikController.cs
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Controllers/EtkinlikController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SfiziAmerica.BusinessLayer.Repository.Concrete;
+using SfiziAmerica.DataAccessLayer.ModelContext;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SfiziAmerica.WebUIandUX.Controllers
 {
     public class EtkinlikController : Controller
     {
-        public IActionResult Index()
+        private DbContext context = new SfizilDatabaseModelContext();
+        private UnitOfWork unitOfWork;
+        private SfizilDatabaseModelContext sfizilDatabase = new SfizilDatabaseModelContext();
+        public EtkinlikController()
         {
-            return View();
+            unitOfWork = new(context);
+        }
+
+        [Route("event")]
+        public async Task<IActionResult> Index()
+        {
+            var model = await sfizilDatabase.Events.Where(x => x.IsActive == true).OrderByDescending(x => x.ID).ToListAsync();
+            return View(model);
         }
     }
 }
