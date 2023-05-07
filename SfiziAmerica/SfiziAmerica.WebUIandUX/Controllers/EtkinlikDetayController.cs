4e959e3ef91c0ffdc3688e68b0fbec48a5cf6f93
360MEKA <83185397+bnymnclskn01@users.noreply.github.com>
2023-05-07 20:26:16 +0300

    asd

---
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Controllers/EtkinlikDetayController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SfiziAmerica.BusinessLayer.Repository.Concrete;
+using SfiziAmerica.DataAccessLayer.ModelContext;
+using SfiziAmerica.WebUIandUX.Models.ViewModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SfiziAmerica.WebUIandUX.Controllers
+{
+    public class EtkinlikDetayController : Controller
+    {
+        private DbContext context = new SfizilDatabaseModelContext();
+        private UnitOfWork unitOfWork;
+        private SfizilDatabaseModelContext sfizilDatabase = new SfizilDatabaseModelContext();
+        public EtkinlikDetayController()
+        {
+            unitOfWork = new(context);
+        }
+
+        [Route("event/{SeoLink}")]
+        public async Task<IActionResult> Index(string SeoLink)
+        {
+            var model = await unitOfWork.eventRepository.GetAsync(x => x.Slug == SeoLink);
+            var list=await sfizilDatabase.Events.Where(x=>x.IsActive==true && x.Slug!=SeoLink).OrderBy(x=>x.CreateDate).ToListAsync();
+            var about = await unitOfWork.aboutRepository.GetAsync(x => x.IsActive == true);
+            var socialMedai = await unitOfWork.socialMediaRepository.GetAllAsync(x => x.IsActive == true);
+            EventDetail eventDetail = new() { Event = model, Events = list, About = about, SocialMedias = socialMedai };
+            return View(eventDetail);
+        }
+    }
+}
---
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Models/ViewModel/EventDetail.cs
@@ -0,0 +1,14 @@
+﻿using SfiziAmerica.EntityLayer.Model;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SfiziAmerica.WebUIandUX.Models.ViewModel
+{
+    public class EventDetail
+    {
+        public IList<Event> Events { get; set; }
+        public Event Event { get; set; }
+        public About About { get; set; }
+        public IList<SocialMedia> SocialMedias { get; set; }
+    }
+}
