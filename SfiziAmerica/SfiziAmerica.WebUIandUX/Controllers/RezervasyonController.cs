3a91a2f93a75fbc249d4ff459c71522b5d7420a1
360MEKA <83185397+bnymnclskn01@users.noreply.github.com>
2023-05-08 09:36:13 +0300

    asd

--- SfiziAmerica/SfiziAmerica.WebUIandUX/Controllers/RezervasyonController.cs
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Controllers/RezervasyonController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SfiziAmerica.BusinessLayer.Repository.Concrete;
+using SfiziAmerica.DataAccessLayer.ModelContext;
+using SfiziAmerica.WebUIandUX.Models.ViewModel;
+using System.Threading.Tasks;
 
 namespace SfiziAmerica.WebUIandUX.Controllers
 {
     public class RezervasyonController : Controller
     {
-        public IActionResult Index()
+        private DbContext context = new SfizilDatabaseModelContext();
+        private UnitOfWork unitOfWork;
+        private SfizilDatabaseModelContext sfizilDatabase = new SfizilDatabaseModelContext();
+        public RezervasyonController()
         {
-            return View();
+            unitOfWork = new(context);
+        }
+        [Route("reservation")]
+        public async Task<IActionResult> Index()
+        {
+            var contactInformation = await unitOfWork
+                           .contactInformationRepository
+                           .GetAllAsync(x => x.IsActive == true);
+            var socialMedia = await unitOfWork
+                .socialMediaRepository
+                .GetAllAsync(x => x.IsActive == true);
+            ContactViewModel contactViewModel = new() { ContactInformations = contactInformation, SocialMedias = socialMedia };
+            return View(contactViewModel);
         }
     }
 }
