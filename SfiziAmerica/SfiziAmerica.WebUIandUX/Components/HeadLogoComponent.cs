cbc06af5b58cea065fb833a3d56c20d0bc6aff79
360MEKA <83185397+bnymnclskn01@users.noreply.github.com>
2023-05-13 23:38:18 +0300

    Düzenleme Alındı

---
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Components/HeadLogoComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SfiziAmerica.DataAccessLayer.ModelContext;
+using System.Linq;
+
+namespace SfiziAmerica.WebUIandUX.Components
+{
+    public class HeadLogoComponent : ViewComponent
+    {
+        private SfizilDatabaseModelContext sfizilDatabase = new SfizilDatabaseModelContext();
+        public IViewComponentResult Invoke()
+        {
+            var model = sfizilDatabase.Logos.Where(x => x.IsActive == true && x.Title == "Üst Logo").FirstOrDefault();
+            return View(model);
+        }
+    }
+}
