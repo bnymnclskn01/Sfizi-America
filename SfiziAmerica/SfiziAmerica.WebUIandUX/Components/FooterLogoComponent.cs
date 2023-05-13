7706df7b0c5e292abda502cdc1a79d4a49407058
360MEKA <83185397+bnymnclskn01@users.noreply.github.com>
2023-05-13 23:45:49 +0300

    Düzenleme Alındı

---
+++ SfiziAmerica/SfiziAmerica.WebUIandUX/Components/FooterLogoComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SfiziAmerica.DataAccessLayer.ModelContext;
+using System.Linq;
+
+namespace SfiziAmerica.WebUIandUX.Components
+{
+    public class FooterLogoComponent : ViewComponent
+    {
+        private SfizilDatabaseModelContext sfizilDatabase = new SfizilDatabaseModelContext();
+        public IViewComponentResult Invoke()
+        {
+            var model = sfizilDatabase.Logos.Where(x => x.IsActive == true && x.Title == "Footer Logo").FirstOrDefault();
+            return View(model);
+        }
+    }
+}
